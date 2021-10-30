4443b61c4918a1dd68126d15e98df1b54698a64d
Rafael C. Viana <rchagas.sdi@gmail.com>
2021-10-30 12:31:15 -0300

    arquivos-base-projeto

---
+++ src/main/java/api/loja/rrocks/ApiLojaDeAplicativosRogaRocksApplication.java
@@ -0,0 +1,13 @@
+package api.loja.rrocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaDeAplicativosRogaRocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaDeAplicativosRogaRocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/loja/rrocks/ApiLojaDeAplicativosRogaRocksApplicationTests.java
@@ -0,0 +1,13 @@
+package api.loja.rrocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaDeAplicativosRogaRocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
