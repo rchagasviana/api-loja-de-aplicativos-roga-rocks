72d398695a8086b00be31d6398dce11fc29d29bf
Rafael C. Viana <rchagas.sdi@gmail.com>
2021-11-03 00:43:53 -0300

    cabeçalho-documentação-ok

--- src/main/java/api/loja/rrocks/config/Configuracao.java
+++ src/main/java/api/loja/rrocks/config/Configuracao.java
@@ -7,20 +7,9 @@ import api.loja.rrocks.entidades.enums.StatusAplicativo;
 import api.loja.rrocks.repositorios.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.transaction.Transactional;
 import java.time.Instant;
 import java.util.Arrays;
 
